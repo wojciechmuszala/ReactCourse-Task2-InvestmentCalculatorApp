bee66817986c74b385f609e77db89d424cef6f74
Wojciech Muszala <wojciech.muszala@gmail.com>
2025-01-14 18:54:48 +0100

    Feature: Add result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Result from "./components/Result";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main>
         <UserInput />
+        <Result />
       </main>
     </>
   );
