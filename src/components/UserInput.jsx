849f6abeb4d5d0f5111713b57d6d7bfe035c847c
Wojciech Muszala <wojciech.muszala@gmail.com>
2025-01-14 21:03:03 +0100

    Feature: Collect input data to state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,57 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  const [userInputValues, setUserInputValues] = useState(null);
+
+  const getUserInputValues = (event) => {
+    const { id, value } = event.target;
+    setUserInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [id]: value,
+    }));
+  };
+
   return (
     <section id='user-input'>
+      {userInputValues && (
+        <ul>
+          {Object.entries(userInputValues).map(([key, value]) => (
+            <li key={key}>
+              {key}: {value}
+            </li>
+          ))}
+        </ul>
+      )}
       <div className='input-group'>
         <p>
-          <label for='initial-investment'>Initial investment</label>
-          <input id='initial-investment' type='number' />
+          <label htmlFor='initial-investment'>Initial investment</label>
+          <input
+            id='initial-investment'
+            type='number'
+            onInput={getUserInputValues}
+          />
         </p>
         <p>
-          <label for='annual-investiment'>Annual investiment</label>
-          <input id='annual-investiment' type='number' />
+          <label htmlFor='annual-investiment'>Annual investiment</label>
+          <input
+            id='annual-investiment'
+            type='number'
+            onInput={getUserInputValues}
+          />
         </p>
       </div>
       <div className='input-group'>
         <p>
-          <label for='expected-return'>Expected return</label>
-          <input id='expected-return' type='number' />
+          <label htmlFor='expected-return'>Expected return</label>
+          <input
+            id='expected-return'
+            type='number'
+            onInput={getUserInputValues}
+          />
         </p>
         <p>
-          <label for='duration'>Duration</label>
-          <input id='duration' type='number' />
+          <label htmlFor='duration'>Duration</label>
+          <input id='duration' type='number' onInput={getUserInputValues} />
         </p>
       </div>
     </section>
